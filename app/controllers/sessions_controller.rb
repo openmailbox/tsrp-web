eeeb3981ad9249ab3442a3a3de9b4a884510662f
Brandon R <brice84@gmail.com>
2022-05-21 16:10:10 -0400

    Allow logins for existing users.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,11 +8,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    #user_info = request.env['omniauth.auth']
-    redirect_to root_url, notice: "Logged in."
+    user_info = request.env['omniauth.auth']
+    auth      = Authorization.find_by(provider: user_info.provider, uid: user_info.uid)
+
+    if auth then
+      session[:current_user_id] = auth.user_id
+      redirect_to root_url, notice: "Logged in."
+    else
+      redirect_to root_url, notice: "You must join our Discord before registering on the website."
+    end
   end
 
   def destroy
+    session[:current_user_id] = nil
     redirect_to root_url, notice: "Logged out."
   end
 end
