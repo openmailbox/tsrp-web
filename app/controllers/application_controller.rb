cbb09a8be032514ef7da08727ad83aa83eb398d3
Brandon R <brice84@gmail.com>
2022-05-21 21:30:26 -0400

    Fix spacing.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-    def current_user
-        @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
-    end
+  def current_user
+    @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+  end
 end
