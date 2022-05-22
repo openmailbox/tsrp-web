497b5e725ec0450c68302785bfa9e5eb035c0552
Brandon R <brice84@gmail.com>
2022-05-21 22:41:01 -0400

    Require authentication in front of /fuel.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,16 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
   def current_user
     @current_user ||= session[:current_user_id] && User.find_by(id: session[:current_user_id])
+  end
+
+  private
+
+  def require_login
+    unless current_user then
+      redirect_to root_url, notice: "You must be logged in."
+      return
+    end
   end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,6 @@
 class SessionsController < ApplicationController
+  skip_before_action :require_login
+
   if Rails.env.development?
     skip_before_action :verify_authenticity_token, only: :create
   end
